9dd43703f653a47694b86099e860d75ead2be14e
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-06 17:05:48 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -16,8 +16,8 @@ class Pokemon
     stm.bind_param 1, id
     rs = stm.execute
     row = rs.next
-    binding.pry
     new_object = Pokemon.new(row[0],row[1],row[2],db)
+    binding.pry
     new_object
   end
 end
