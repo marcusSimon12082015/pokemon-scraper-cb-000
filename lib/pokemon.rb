24c1b66ea69c4fad5702fd02abe7b880a1fe85a2
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-06 18:04:26 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -22,6 +22,6 @@ class Pokemon
     new_object
   end
   def alter_hp(health,db)
-     db.execute("UPDATE pokemon SET hp = ? WHERE id = ?",health,self.id)
+     row = db.execute("UPDATE pokemon SET hp = ? WHERE id = ?",health,self.id)
   end
 end
